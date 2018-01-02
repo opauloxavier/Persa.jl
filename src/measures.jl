5c1def3dc93dd977f973909fdda4edbbf8a4fe11
Filipe Braida <filipebraida@gmail.com>
2018-01-02 00:01:53 -0200

    Creating class decision measure

--- src/measures.jl
+++ src/measures.jl
@@ -117,3 +117,112 @@ function DataFrame(result::CFMetrics...)
 
   return df
 end
+
+struct ClassDecisionMeasures <: CFMetrics
+    precision::Dict
+    recall::Dict
+    f1::Dict
+    macrof1::AbstractFloat
+end
+
+aval{T <: CFModel}(model::T, data_test::Array, preferences::RatingPreferences) = ClassDecisionMeasures(model, data_test, preferences)
+
+function ClassDecisionMeasures(model::CFModel, data_test::Array, preferences::RatingPreferences)
+  predicts = Persa.predict(model, data_test)
+
+  #TODO: Better this code
+  values = zeros(eltype(preferences), length(predicts), 1)
+
+  i = 0
+  for i = 1:length(predicts)
+      if !isnan(predicts[i])
+          values[i] = round(predicts[i], preferences)
+      end
+  end
+
+  nonnanindex = find(r->!isnan(r), predicts)
+
+  return ClassDecisionMeasures(data_test[nonnanindex, 3], values[nonnanindex], unique(preferences))
+end
+
+function ClassDecisionMeasures(labels::Array, predict::Array, preferences::Array)
+    preferencesmap = Dict{eltype(preferences), Int}()
+
+    for i in preferences
+        preferencesmap[i] = length(preferencesmap) + 1
+    end
+
+    confusion = zeros(Int, length(preferences), length(preferences))
+
+    for i = 1:length(labels)
+        confusion[preferencesmap[labels[i]], preferencesmap[predict[i]]] += 1
+    end
+
+    precisionmap = Dict{eltype(preferences), AbstractFloat}()
+    recallmap = Dict{eltype(preferences), AbstractFloat}()
+    f1map = Dict{eltype(preferences), AbstractFloat}()
+
+    for preference in preferences
+        index = preferencesmap[preference]
+
+        precision = confusion[index, index] ./ (sum(confusion[:, index]))
+        recall = confusion[index, index] ./ (sum(confusion[index, :]))
+
+        if isnan(precision) || isinf(precision)
+            precision = 0
+        end
+
+        if isnan(recall) || isinf(recall)
+            recall = 0
+        end
+
+        f1 = (2 * precision * recall) / (precision + recall)
+
+        if isnan(f1) || isinf(f1)
+            f1 = 0
+        end
+
+        precisionmap[preference] = precision
+        recallmap[preference] = recall
+        f1map[preference] = f1
+    end
+
+    macroprecision = sum(values(precisionmap)) / length(preferences)
+    macrorecall = sum(values(recallmap)) / length(preferences)
+
+    macrof1 = (2 * macroprecision * macrorecall) / (macroprecision + macrorecall)
+
+    return ClassDecisionMeasures(precisionmap, recallmap, f1map, macrof1)
+end
+
+function DataFrame(result::ClassDecisionMeasures)
+    df = DataFrame()
+    
+    ratings = collect(keys(result.precision))
+    sort!(ratings)
+
+    for rating in ratings
+        df[Symbol("precision_$(rating)")] = result.precision[rating]
+        df[Symbol("recall_$(rating)")] = result.recall[rating]
+        df[Symbol("f1_$(rating)")] = result.f1[rating]
+    end
+
+    df[:macrof1] = result.macrof1
+    return df
+end
+
+function Base.print(result::ClassDecisionMeasures)
+    ratings = collect(keys(result.precision))
+    sort!(ratings)
+
+    for rating in ratings
+        println("Rating $rating:")
+        println("Precision - $(result.precision[rating])")
+        println("Recall - $(result.recall[rating])")
+        println("F1 - $(result.f1[rating])")
+        println("")
+    end
+
+    println("Global:")
+    println("Macro F1 - $(result.macrof1)")
+end
